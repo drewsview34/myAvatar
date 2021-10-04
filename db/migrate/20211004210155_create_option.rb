31e0af30e10e58a9226f373b45bc3b5ad2a7781d
drewsview34 <drewsview34@outlook.com>
2021-10-04 14:02:38 -0700

    created migration file for option db

---
+++ db/migrate/20211004210155_create_option.rb
@@ -0,0 +1,11 @@
+class CreateOption < ActiveRecord::Migration[6.1]
+  def change
+    create_table :options do |t|
+      t.string :name
+      t.string :super_power
+      t.string :super_weakness
+
+      t.timestamps
+    end
+  end
+end
