753767c856a40959137bfb920f50c03ce30b8710
drewsview34 <drewsview34@outlook.com>
2021-10-04 14:27:50 -0700

    edited controller file name

--- app/controllers/avatar_controller.rb
+++ app/controllers/avatar_controller.rb
@@ -1,4 +1,4 @@
-class AvatarsController < ApplicationController
+class AvatarController < ApplicationController
 
     def index
 
--- app/controllers/option_controller.rb
+++ app/controllers/option_controller.rb
@@ -1,4 +1,4 @@
-class OptionsController < ApplicationController
+class OptionController < ApplicationController
 
     def create
 
