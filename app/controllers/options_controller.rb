26984b67d530ca49cc3d5f6e940e1b0006e134cb
drewsview34 <drewsview34@outlook.com>
2021-10-05 12:35:39 -0700

    created actions methods

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,33 +1,38 @@
 class OptionsController < ApplicationController
 
     def index
-
+        @options = Option.all
     end
 
     def new
-
+        @option = Option.new
     end
 
     def edit
-
+        @option = Option.find(params[:id])
     end
 
     def update
-
+        @option = Option.find(params[:id])
+        @option.update(option_params)
+        redirect_to option_path(@option)
     end
 
     def create
-
+        @option = Option.create(option_params)
+        redirect_to option_path(@option)
     end
 
     def show
-
+        @user = User.find(session[:user_id])
+        @option = Option.find(params[:id])
+        @avatar = @option.avatars.build(user_id: @user_id)
     end
 
     
     private
 
-    def attraction_params
+    def option_params
         params.require(:attraction).permit(:name, :super_power, :super_weakness)
     end
 
