a16006c3b0289d27cbd8660907f280bccbedee88
drewsview34 <drewsview34@outlook.com>
2021-10-05 12:47:15 -0700

    added action methods to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,33 @@
 class UsersController < ApplicationController
 
+    def new
+        @user = User.new
+    end
 
     def create
-
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id]  = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
+
+    def show
+        if session[:user_id]
+            current_user
+        else
+            redirects_to root_path
+        end
+    end
+
 
     
-    def show
+    private
 
-    end
-
-    def destroy
-
+    def user_params
+        params.require(:user).permit(:name, :password, :)
     end
 
 end
