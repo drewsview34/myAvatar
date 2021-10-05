408cf949199ac6c99593f55b5b8efdbaeaa095c0
drewsview34 <drewsview34@outlook.com>
2021-10-05 13:59:03 -0700

    added root route and action routes for signin, sessions, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'welcome#home'
+
+  get 'signin', to: 'sessions#signin'
+  post '/signin', to: 'sessions#signin'
+  get 'sessions/new', to: 'sessions#new'
+  get '/sessions/create', to: 'sessions#create'
+  get '/sessions/destroy', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :avatar
   resources :option
   resources :user
